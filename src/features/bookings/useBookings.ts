ef77c1215250427dc94dafd8f4368a401e9c12a9
HelenaIsh <elena-ishmatova@mail.ru>
2025-05-22 16:38:46 +0200

    feat: add prefetching

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -1,9 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
 import { useSearchParams } from 'react-router-dom';
+import { PAGE_SIZE } from '../../utils/constants';
 
 export const useBookings = () => {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   const filterValue = searchParams.get('status');
   const filter =
@@ -25,6 +27,20 @@ export const useBookings = () => {
     queryKey: ['bookings', filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  const pageCount = Math.ceil(count ? count / PAGE_SIZE : 0);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return { bookings, isLoading, error, count };
 };
