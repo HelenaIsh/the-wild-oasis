658f8b85f0de4ef17cc0ede2a1db4b287ceee0ca
HelenaIsh <elena-ishmatova@mail.ru>
2025-05-27 11:31:16 +0200

    feat: add protected routes

---
+++ src/ui/ProtectedRoute.tsx
@@ -0,0 +1,33 @@
+import { useEffect, type ReactNode } from 'react';
+import { useUser } from '../features/authentication/useUser';
+import Spinner from './Spinner';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default function ProtectedRoute({ children }: { children: ReactNode }) {
+  const { isLoading, isAuthenticated } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) {
+      navigate('/login');
+    }
+  }, [isAuthenticated, isLoading, navigate]);
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  return children;
+}
