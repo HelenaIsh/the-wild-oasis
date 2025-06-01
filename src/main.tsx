db80f1b33acb78f0c0b719a1ddf8a7e2d32d4010
HelenaIsh <elena-ishmatova@mail.ru>
2025-06-01 19:14:03 +0200

    feat: add error boundaries

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './ui/ErrorFallback.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace('/')}
+    >
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
