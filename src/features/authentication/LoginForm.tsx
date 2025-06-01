49fac0e38a751caf73df628c56978517abcaef61
HelenaIsh <elena-ishmatova@mail.ru>
2025-06-01 19:54:27 +0200

    Revert "fix: delete test credentials"
    This reverts commit e830d981670cec0c8412d5f23dbf59dcf34af6ed.

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -8,8 +8,8 @@ import { useLogin } from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('jonas@example.com');
+  const [password, setPassword] = useState('Qwerty1');
   const { login, isPending } = useLogin();
 
   function handleSubmit(e: React.FormEvent) {
